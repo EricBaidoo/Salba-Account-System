903ae02380ceda094a7bebd6c7aeeda77928911c
Eric Baidoo <kbaidu7@gmail.com>
2026-04-17 13:13:00 +0000

    chore: remove vendor directory from gitignore and enable error reporting in login.php

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
 <?php
+// TEMPORARY: Enable error reporting for live debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if (session_status() === PHP_SESSION_NONE) session_start();
 require_once __DIR__ . '/includes/auth_functions.php';
 include __DIR__ . '/includes/db_connect.php';
